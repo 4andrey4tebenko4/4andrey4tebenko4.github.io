148f2710c33a969eb951d154b1fbf2fcacaa84f0
Andrey Tebenko <57828230+4andrey4tebenko4@users.noreply.github.com>
2021-02-27 21:17:27 +0200

    Add files via upload

---
+++ myProject/js/slider.js
@@ -0,0 +1,20 @@
+$(function(){
+  $('.sliderItems').slick({
+    arrows: false,
+    slidesToShow: 3,
+    autoplay: true,
+    autoplaySpeed: 200,
+    speed:1000
+  })
+})
+
+$(function(){
+  $('.cardsStories').slick({
+    arrows: false,
+    slidesToShow: 3,
+    autoplay: true,
+    dots: true,
+    autoplaySpeed: 300,
+    speed:800
+  })
+})
